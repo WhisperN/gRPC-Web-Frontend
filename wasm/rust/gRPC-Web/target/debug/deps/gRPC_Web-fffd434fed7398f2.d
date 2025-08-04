72d88dac27421c5fd4cc3abaeafcfdf857289676
Nils Jacobi <76872969+WhisperN@users.noreply.github.com>
2025-08-04 14:39:00 +0200

    Basic setup

---
+++ wasm/rust/gRPC-Web/target/debug/deps/gRPC_Web-fffd434fed7398f2.d
@@ -0,0 +1,5 @@
+/Users/nils/Documents/UZH/FS25/bsc/gRPC-Web-Frontend/wasm/rust/gRPC-Web/target/debug/deps/gRPC_Web-fffd434fed7398f2.d: src/main.rs
+
+/Users/nils/Documents/UZH/FS25/bsc/gRPC-Web-Frontend/wasm/rust/gRPC-Web/target/debug/deps/libgRPC_Web-fffd434fed7398f2.rmeta: src/main.rs
+
+src/main.rs:
